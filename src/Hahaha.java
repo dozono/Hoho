08ad8345add43a9859639de7cc85b4337737272a
Hao <chrislee.hao@gmail.com>
2016-04-01 21:10:44 -0600

    haha

---
+++ src/Hahaha.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Hao on 4/1/2016.
+ */
+public class Hahaha
+{
+	public static void main(String[] args){
+		System.out.println("If you see this, fuck you. And btw, you are retard.");
+	}
+}
